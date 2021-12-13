bb1a0a2c2dcd640778e3d0a034b85b8a74de811d
Dominik Reuting <dominik.reuting@gmail.com>
2021-12-13 23:15:51 +0100

    feat: add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+    GraphQLModule.forRoot({}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
