75eebc71b9411a30d04b3724649173997dc30937
Dominik Reuting <dominik.reuting@gmail.com>
2021-12-15 23:53:16 +0100

    feat: graphql auth

---
+++ src/user/user.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserResolver } from './user.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './user.entity';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      // TODO: Use ENV secret 
+      secret: 'hard!to-guess_secret',
+      signOptions: { expiresIn: '24h' },
+    }),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UserResolver, UserService, JwtStrategy],
+})
+export class UserModule {}
